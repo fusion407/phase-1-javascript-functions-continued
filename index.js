3f15fa5f64621340a87854aef756e48ac60c15a8
fusion407 <fusion407@gmail.com>
2022-06-28 21:58:47 -0500

    commit tree f22f609cb96044a60e35174430d9e3340d3df3ab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+function wrapAdjective(symbol) {
+    const innerFunction = function(adj = "a dedicated programmer"){
+        return `You are ${symbol}${adj}${symbol}!`;
+    }
+    return innerFunction;
+}
